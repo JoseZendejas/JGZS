893a07f30c6cac19691aae6b5f06cc8b4e4e7c0a
josen <josen@DESKTOP-8QGCGSE.lan>
2021-11-15 17:18:30 -0600

    Se creó y programó la clase "Grafo"

---
+++ src/Grafo.java
@@ -0,0 +1,26 @@
+
+import javax.swing.JFrame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author josen
+ */
+public class Grafo {
+    
+    
+  public static void main (String [] args){  
+  JFrame ventana= new JFrame ("GRAFO") ;
+  ventana.add(new Lienzo());
+  ventana.setSize(600,600);
+  ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  ventana.setVisible(true);
+  
+  
+  }
+}
